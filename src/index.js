0ff6294aeeeb569bb06dea61599f8a28bff24fec
sergey.koval <sergey.koval@hrb.software>
2023-08-09 12:25:29 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  const openBrackets = [];
+
+  function checkBracket(bracket) {
+    const bracketGroup = bracketsConfig.find(it => it.includes(bracket));
+
+    if (bracketGroup[0] === bracketGroup[1]) {
+      if (openBrackets.includes(bracketGroup[0])) {
+        openBrackets.pop();
+      } else {
+        openBrackets.push(bracket);
+      }
+    } else {
+      if (bracket === bracketGroup[0]) {
+        openBrackets.push(bracket);
+      } else if (bracket === bracketGroup[1]) {
+        if (
+          openBrackets.length &&
+          openBrackets[openBrackets.length - 1] === bracketGroup[0]
+        ) {
+          openBrackets.pop();
+        }
+      }
+    }
+  }
+
+  for (let s of str) {
+    checkBracket(s);
+  }
+
+  return !openBrackets.length;
+};
